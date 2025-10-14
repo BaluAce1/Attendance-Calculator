e282330ff05c545b26818a103fa159afbc4504d9
BaluAce1 <148438948+BaluAce1@users.noreply.github.com>
2025-10-14 19:19:20 +0530

    Update Main.java
    A more user friendly and re usable code using OOP Programming

--- Main.java
+++ Main.java
@@ -1,216 +1,214 @@
-import java.util.Scanner;
-
-public class Main {
-public static void main (String[] args) {
-System.out.println("Welcome to Attendance Calculator ");
-System.out.println("Enter your attedance format ");
-System.out.println("1.Periods");
-System.out.println("2.Days");
-Scanner sc = new Scanner(System.in);
-int input = sc.nextInt();
-
-while(input != 1 && input != 2)  
-{  
-    System.out.println("Invalid input !\nEnter correct input:");  
-    System.out.println("1.Periods");  
-    System.out.println("2.Days");  
-    input = sc.nextInt();  
-}  
+import java.text.DecimalFormat;
+ import java.util.Scanner;
+ class Calc {
+ double prcnt = 0.00;
+ private int total = 0;
+ private int attended = 0;
+ private int periods1 = 0;
+ private boolean periods = true;
+ void cal(int num1,int num2,boolean periods,int periods1) {
+ this.total = num1;
+ this.attended = num2;
+ this.periods = periods;
+ this.periods1 = periods1;
+ }
+ public void AttdCalc() {
+ double prcnt = ((double) attended / total) * 100;
+ this.prcnt = prcnt;
+ DecimalFormat df = new DecimalFormat("#.00");
+ String prcnt_str = df.format(prcnt);
+ System.out.println("Your Attendance is " + prcnt_str + " % ");
  
-switch(input) {  
-    case 1 :  
-        System.out.println("Enter your total periods :");  
-        int Total_periods = sc.nextInt();  
-        System.out.println("Enter attended periods :");  
-        int Attended_periods = sc.nextInt();  
-        System.out.println("Enter the number of periods in a day :");  
-        int periods = sc.nextInt();  
-        float Attendance = ((float) Attended_periods / Total_periods) * 100 ;  
-        System.out.println("Your attendance = " + Attendance);  
-        System.out.println("\nWanna know how many days you have to attend to pass the requirement ");  
-        System.out.println("1.Yes");  
-        System.out.println("2.Exit");  
-        int input2 = sc.nextInt();  
-        while(input2 != 1 && input2 != 2) {  
-            System.out.println("You have entered Wrong input ");  
-            System.out.println("1.Yes");  
-            System.out.println("2.Exit");  
-            input2 = sc.nextInt();  
-        }  
-        if(input2 == 1){  
-            System.out.println("Enter your Attedance Requirement % :");  
-            int requirement = sc.nextInt();  
-            if(Attendance >= requirement ) {  
-                System.out.println("You have attendance more than requirement already ");  
-                 
-            }  
-            else {  
-             
-            int time_required = ((requirement * Total_periods) - (100 * Attended_periods ) ) / (100 - requirement);  
-            int days_required = (time_required / periods);  
-            int periods_required = (time_required % periods);  
-            System.out.println("You have to attend " + days_required + " days " + periods_required + " periods " );
-            System.out.println(" Welcome to Attendance Predictor : \n");
-            System.out.println(" You can calculate how much your attendance % increases after attending input days given ");
-            System.out.println("You can calculate how much your attendance % will decrease if you skip given input days ");
-            System.out.println("\nContinue ?  ");
-            System.out.println("1.Yes");
-            System.out.println("2.No");
-            int input3 = sc.nextInt();
-            while(input3 != 1 && input3 != 2){
-            System.out.println("Invalid Input");
-            System.out.println(" Welcome to Attendance Predictor : \n");
-            System.out.println(" You can calculate how much your attendance % increases after attending input days given ");
-            System.out.println("You can calculate how much your attendance % will decrease if you skip given input days ");
-            System.out.println("\nContinue ?  ");
-            System.out.println("1.Yes");
-            System.out.println("2.No");
-            input3 = sc.nextInt();
-             }
-             if(input3 == 1 ) {
-            System.out.println("Please Choose the apropriate option ");
-            System.out.println("1.Attendance increment After n days ");
-            System.out.println("2.Attendance reduction after n days ");
-            int input5 = sc.nextInt();
-            while(input5 != 1 && input5 != 2){
-                System.out.println("Incorrect Input ");
-                System.out.println("Please Choose the apropriate option ");
-                System.out.println("1.Attendance increment After n days ");
-                System.out.println("2.Attendance reduction after n days ");
-                input5 = sc.nextInt();
-            }
-            switch(input5) {
-                case 1 :
-                    System.out.println("Enter the no of days you will attend ");
-                    int i = sc.nextInt();
-                
-                            int New_Total = Total_periods + (i * periods);
-                            int New_Attended = Attended_periods + (i * periods);
-                            float New_Attendance = ((float) New_Attended / New_Total) * 100;
-                            System.out.println("Increased Attendance after  " + i + "  days  = " + New_Attendance +  " %");
-                            break ;
-                            
-                default : 
-                        System.out.println("Enter the no of days you plan to bunk ");
-                        i = sc.nextInt();
-                
-                             New_Total = Total_periods + (i * periods);
-                             New_Attended = Attended_periods;
-                             New_Attendance = ((float) New_Attended / New_Total) * 100;
-                            System.out.println("Decreased Attendance after  " + i + "  days bunk  = " + New_Attendance +  " %");
-                         
-                    }
-            }
-            else {
-                System.out.println("Exiting..." );
-                System.exit(0);
-            }
-            } 
-        }  
-        else {  
-            System.out.println("exiting system");  
-            System.exit(0);  
-             }
-        break;  
-     
-    default :  
-        System.out.println("Enter your total Days");  
-        int Total_Days = sc.nextInt();  
-        System.out.println("Enter attended Days");  
-        int Attended_Days = sc.nextInt();  
-        Attendance = ((float) Attended_Days / Total_Days) * 100 ;  
-        System.out.println("Your attendance = " + Attendance);  
-        System.out.println("\nWanna know how many days you have to attend to pass the requirement ");  
-        System.out.println("1.Yes");  
-        System.out.println("2.Exit");  
-         input2 = sc.nextInt();  
-        while(input2 != 1 && input2 != 2) {  
-            System.out.println("You have entered Wrong input ");  
-            System.out.println("1.Yes");  
-            System.out.println("2.Exit");  
-            input2 = sc.nextInt();  
-        }  
-        if(input2 == 1){  
-            System.out.println("Enter your Attedance Requirement % :");
-           
-            int requirement = sc.nextInt();  
-            System.out.println("Enter the total periods in a day " );
-            periods = sc.nextInt();
-            if(Attendance >= requirement ) {  
-                System.out.println("You have attendance more than requirement already ");  
-            }  
-            else {  
-               Total_periods = Total_Days * periods;
-               Attended_periods = Attended_Days * periods ;
-              int time_required = ((requirement * Total_periods) - (100 * Attended_periods ) ) / (100 - requirement);  
-              int days_required = (time_required / periods);  
-              int periods_required = (time_required % periods);  
-            System.out.println("You have to attend " + days_required + " days " + periods_required + " periods " );
-            
-            System.out.println(" Welcome to Attendance Predictor : \n");
-            System.out.println(" You can calculate how much your attendance % increases after attending input days given ");
-            System.out.println("You can calculate how much your attendance % will decrease if you skip given input days ");
-            System.out.println("\nContinue ?  ");
-            System.out.println("1.Yes");
-            System.out.println("2.No");
-            int input3 = sc.nextInt();
-            while(input3 != 1 && input3 != 2){
-            System.out.println("Invalid Input");
-            System.out.println(" Welcome to Attendance Predictor : \n");
-            System.out.println(" You can calculate how much your attendance % increases after attending input days given ");
-            System.out.println("You can calculate how much your attendance % will decrease if you skip given input days ");
-            System.out.println("\nContinue ?  ");
-            System.out.println("1.Yes");
-            System.out.println("2.No");
-            input3 = sc.nextInt();
-             }
-             if(input3 == 1 ) {
-            System.out.println("Please Choose the apropriate option ");
-            System.out.println("1.Attendance increment After n days ");
-            System.out.println("2.Attendance reduction after n days ");
-            int input5 = sc.nextInt();
-            while(input5 != 1 && input5 != 2){
-                System.out.println("Incorrect Input ");
-                System.out.println("Please Choose the apropriate option ");
-                System.out.println("1.Attendance increment After n days ");
-                System.out.println("2.Attendance reduction after n days ");
-                input5 = sc.nextInt();
-            }
-            switch(input5) {
-                case 1 :
-                    System.out.println("Enter the no of days you will attend ");
-                    int i = sc.nextInt();
-                
-                            int New_Total = Total_periods + (i * periods);
-                            int New_Attended = Attended_periods + (i * periods);
-                            float New_Attendance = ((float) New_Attended / New_Total) * 100;
-                            System.out.println("Increased Attendance after  " + i + "  days  = " + New_Attendance +  " %");
-                            break ;
-                            
-                default : 
-                        System.out.println("Enter the no of days you plan to bunk ");
-                        i = sc.nextInt();
-                
-                             New_Total = Total_periods + (i * periods);
-                             New_Attended = Attended_periods;
-                             New_Attendance = ((float) New_Attended / New_Total) * 100;
-                            System.out.println("Decreased Attendance after  " + i + "  days bunk  = " + New_Attendance +  " %");
-                         
-                    }
-            }
-            else {
-                System.out.println("Exiting");
-                System.exit(0);
-            }
-           
-            }  
-         
-}  
-    else {  
-            sc.close();  
-            System.out.println("exiting system");  
-            System.exit(0);  
-         }  
-}
-}
-}
+ 
+ }
+ public boolean custmAttd(int percent) {
+ int days = 0;
+ int priods = 0;
+ int periods_left = 0;
+
+ if(total != 0) { 
+ if(percent == 100) {
+ System.out.println("You need to attend every day conducted ");
+ return true;
+ }
+ else if(percent > 100 || percent <0) {
+ System.out.println("Invalid Input ");
+ return false;
+ }
+ else {
+ if(periods == true ){
+ if(percent < prcnt ) {
+ System.out.println("You already exceed the " + prcnt + " attendance requirement having " + percent + " %");
+ }
+ else {
+ priods = ((percent * total) - (100 * attended)) / (100 - percent);
+ days = priods / periods1;
+ periods_left = priods % periods1;
+ System.out.println("You have to attend " + days + "days and " + periods_left + " periods");
+ }
+ }
+ else {
+ if(percent < prcnt ) {
+ System.out.println("You already exceed the " + prcnt + " attendance requirement having " + percent + " %");
+ }
+ else {
+ priods = ((percent * (total * periods1)) - (100 * (attended * periods1))) / (100 - percent);
+ days = priods / periods1;
+ periods_left = priods % periods1;
+ System.out.println("You have to attend " + days + " days and " + periods_left + " periods");
+ }
+ }
+ }
+ return true;
+ }
+ else {
+ System.out.println("You havent given attendance input yet ");
+ return true;
+ }
+ }
+ public boolean inputhandle(int input){
+ if(input <= 2 && input > 0) {
+ return false;
+ }
+ else {
+ return true;
+ }
+ 
+ }
+ public void Increment(int input ) {
+ double increment = 0;
+ if(periods){
+ increment = ((((double)attended + (input * periods1) ) ) / (total + (input * periods1)) ) * 100; 
+ 
+ 
+ } else {
+ increment = (( ((double)attended * periods1) + (input * periods1) ) / ((total * periods1) + (input * periods1) )) * 100;
+ }
+ DecimalFormat cf = new DecimalFormat("#.00");
+ String increase = cf.format(increment);
+ String increase_perc = cf.format((increment) - prcnt );
+ System.out.println("You will have " + increase + " % ");
+ System.out.println("Increment == + " + increase_perc + " % ");
+ 
+ 
+ }
+ public void Decrement(int input ) {
+ double Decrement = 0;
+ if(periods){
+ Decrement =( ((double)attended ) / (total + (input * periods1)) ) * 100; 
+ 
+ 
+ } else {
+ Decrement = (((double)(attended * periods1)) / ((total * periods1) + (input * periods1) )) * 100;
+ }
+ DecimalFormat bf = new DecimalFormat("#.00");
+ String decrease = bf.format(Decrement);
+ String decrease_perc = bf.format((prcnt-Decrement));
+ System.out.println("You will have " +decrease + " % ");
+ System.out.println("Decrement == - " + decrease_perc + " % ");
+ 
+ 
+ }
+ 
+ 
+ 
+ }
+ public class AtndCalc {
+ public static void main(String[] args) { 
+ Scanner sc = new Scanner(System.in);
+ int input = 1;
+ int edge_case = 1;
+ Calc cal = new Calc();
+ do {
+ System.out.println("Welcome to Attendance Calculator ");
+ System.out.println("1.Attendance input");
+ System.out.println("2.Attendance");
+ System.out.println("3.Days Predictor for Attendance");
+ System.out.println("4.Attendance Increment and Reduction ");
+ System.out.println("0.Exit");
+ input = sc.nextInt();
+ switch(input) {
+ case 1:
+ String name = "n";
+ Boolean cond2 = true;
+ System.out.println("Attendance Format");
+ System.err.println("1.Periods");
+ System.err.println("2.Days");
+ int cond = sc.nextInt();
+ while(cal.inputhandle(cond)) {
+ System.out.println("You have entered Wrong input");
+ System.err.println("Enter again ");
+ cond = sc.nextInt();
+ 
+ }
+ if(cond == 1) {
+ name = "Periods";
+ cond2 = true; 
+ 
+ 
+ }
+ else {
+ name = "Days";
+ cond2 = false;
+ 
+ 
+ } 
+ System.out.println("Enter total " +name );
+ int total = sc.nextInt();
+ System.out.println("Enter Attended " + name);
+ int attended = sc.nextInt();
+ System.out.println("Enter The toal no of periods in a day ");
+ int periods = sc.nextInt();
+ cal.cal(total,attended,cond2,periods);
+ edge_case = 2;
+ break;
+ case 2:
+ if(edge_case != 2) {
+ System.out.println("Please Enter Attendance Input ");
+ }
+ else {
+ cal.AttdCalc();
+ }
+ break;
+ case 3:
+ System.out.println("Enter Attendance Requirement : ");
+ int req = sc.nextInt();
+while(cal.custmAttd(req) == false) {
+ System.out.println("Enter requirement properly ");
+ req = sc.nextInt();
+ }
+ break;
+ case 4:
+ System.out.println("Please Choose required option ");
+ System.out.println("1.Reduction Calculator");
+ System.out.println("2.Increment Calculator");
+ int choice = sc.nextInt();
+ while(cal.inputhandle(choice)) {
+ System.out.println("You have entered Wrong input ");
+ choice = sc.nextInt();
+ }
+ int days = 0;
+ if(choice == 1) {
+ System.out.println("No of days you plan to Bunk : ");
+ days = sc.nextInt();
+ cal.Decrement(days);
+ 
+ }
+ else {
+ System.out.println("No of days you plan to Attend : ");
+ days = sc.nextInt();
+ cal.Increment(days);
+ }
+ 
+ break;
+ default:
+ System.out.println("Please Enter correct Option");
+ 
+ 
+ 
+ }
+ }
+ while(input != 0);
+ sc.close();
+ }
+ }
